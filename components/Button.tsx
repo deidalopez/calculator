5a68c1cd817a31fac6f4544b20c0e1e008bbb56a
Deida Lopez <deidalopez1@gmail.com>
2024-09-15 16:31:23 -0700

    Button styling

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,29 +1,59 @@
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { StyleSheet, TouchableOpacity } from "react-native";
 import { ThemedText } from "./ThemedText";
+type ButtonTypes = "number" | "operator" | "secondary" | "wide";
 
 type ButtonProps = {
   value: string;
-  type: "number" | "operator" | "secondary";
+  type: ButtonTypes;
   onPress: () => void;
 };
 
 export function Button({ value, type, onPress }: ButtonProps) {
+  const getCustomStyle = () => {
+    switch (type) {
+      case "operator":
+        return [{ ...styles.container, ...styles.orange }];
+      case "secondary":
+        return [{ ...styles.container, ...styles.lightGrey }];
+      case "wide":
+        return [{ ...styles.container, ...styles.wide }];
+      default:
+        return styles.container;
+    }
+  };
+
   return (
-    <TouchableOpacity onPress={onPress} style={styles.container}>
-      <ThemedText>{value}</ThemedText>
+    <TouchableOpacity onPress={onPress} style={getCustomStyle()}>
+      <ThemedText style={type == "secondary" && styles.darkText}>
+        {value}
+      </ThemedText>
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    borderColor: "red",
-    borderWidth: 1,
+    flex: 1,
+    maxWidth: 70,
+    maxHeight: 70,
+    borderRadius: 70,
+    backgroundColor: "#323436",
     justifyContent: "center",
     alignItems: "center",
-    width: 70,
-    height: 70,
-    borderRadius: 50,
-    flex: 1,
+  },
+  orange: {
+    backgroundColor: "orange",
+  },
+  lightGrey: {
+    backgroundColor: "lightgrey",
+  },
+  wide: {
+    maxWidth: "40%",
+    flex: 2,
+    alignItems: "flex-start",
+    paddingLeft: 20,
+  },
+  darkText: {
+    color: "black",
   },
 });
