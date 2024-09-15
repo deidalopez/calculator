e5cdbda4ae273a5a0b4c9e5f2dc7b3171dad81f7
Deida Lopez <deidalopez1@gmail.com>
2024-09-15 16:31:46 -0700

    Display and Text styling update

--- components/Display.tsx
+++ components/Display.tsx
@@ -8,7 +8,12 @@ type DisplayProps = {
 export function Display({ value }: DisplayProps) {
   return (
     <View style={styles.container}>
-      <ThemedText>{value}</ThemedText>
+      <ThemedText
+        style={value.length > 10 ? styles.smallerText : styles.text}
+        numberOfLines={1}
+      >
+        {value}
+      </ThemedText>
     </View>
   );
 }
@@ -18,6 +23,19 @@ const styles = StyleSheet.create({
     borderColor: "red",
     borderWidth: 1,
     alignItems: "flex-end",
+    justifyContent: "center",
     width: "90%",
+    minHeight: 80,
+    paddingHorizontal: 25,
+  },
+  text: {
+    fontSize: 70,
+    lineHeight: 90,
+    fontWeight: 300,
+  },
+  smallerText: {
+    fontSize: 40,
+    lineHeight: 56,
+    fontWeight: 300,
   },
 });
