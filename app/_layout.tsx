e317db2ee4020539cc01e7ce8470c171c7269b45
Deida Lopez <deidalopez1@gmail.com>
2024-09-15 16:59:09 -0700

    Use Expo StatusBar and ThemeProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,21 @@
 import { Stack } from "expo-router";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <StatusBar style="light" />
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </ThemeProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
-import { Text, View, StyleSheet } from "react-native";
-import { Display } from "@/components/Display";
+import { View, StyleSheet, SafeAreaView } from "react-native";
+import { MemoizedDisplay as Display } from "@/components/Display";
 import { ButtonGrid } from "@/components/ButtonGrid";
 import { useState } from "react";
 
@@ -7,19 +7,23 @@ export default function Index() {
   const [displayValue, setDisplayValue] = useState("0");
 
   return (
-    <View style={styles.container}>
-      <Display value={displayValue} />
-      <ButtonGrid setDisplay={setDisplayValue} />
-    </View>
+    <SafeAreaView style={styles.flex}>
+      <View style={styles.container}>
+        <Display value={displayValue} />
+        <ButtonGrid setDisplay={setDisplayValue} />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  flex: { flex: 1, backgroundColor: "#0c0d0f" },
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-end",
     alignItems: "center",
-    gap: 10,
-    // backgroundColor: "#0c0d0f",
+    backgroundColor: "#0c0d0f",
+    marginBottom: 60,
+    gap: 12,
   },
 });
